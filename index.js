58baf0bc230993fda1779112c319f59e0e4aa29b
koussay183 <koussaybnit@gmail.com>
2023-11-11 17:00:20 +0100

    adding the main tags

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ app.use(function (req, res, next) {
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
-
 // Handle React routing, return all requests to React app
-app.get('*', async (req, res) => {
+app.get("*",async (req, res) => {
     var global = fs.readFileSync(path.join(__dirname,"build","index.html"),{encoding:"utf-8"});
 
     let final = global;
@@ -32,10 +31,10 @@ app.get('*', async (req, res) => {
             const result = await fetch("https://api.themoviedb.org/3/movie/"+movieId+"?api_key=20108f1c4ed38f7457c479849a9999cc")
             const info = await result.json();
 
-            final = final.replace("__DESCRIPTION__",info?.overview)
-            final = final.replace("__FB_DESCRIPTION__",info?.overview)
-            final = final.replace('__POSTER__',"https://image.tmdb.org/t/p/original/"+info?.backdrop_path)
-            final = final.replace("__FB_TITLE__",info?.title + " | On Moviea Now")
+            final = final.replace("With moviea.tn you can watch free movies online anytime, on any device. It's never been easier to stream movies so get started. | movies | tv shows | movie | free movies | watch free | film complete | film | stream",info?.overview)
+            final = final.replace("With moviea.tn you can watch free movies online anytime, on any device. It's never been easier to stream movies so get started. | movies | tv shows | movie | free movies | watch free | film complete | film | stream",info?.overview)
+            final = final.replace('https://assets.nflxext.com/ffe/siteui/vlv3/b85863b0-0609-4dba-8fe8-d0370b25b9ee/e7e23acb-d3f4-4c16-87da-22b5e0289506/TN-en-20230731-popsignuptwoweeks-perspective_alpha_website_large.jpg',"https://image.tmdb.org/t/p/original/"+info?.backdrop_path)
+            final = final.replace("Watch Movies | Tv-Shows Online Free",info?.title + " | On Moviea Now")
             
             res.send(final);
           }
@@ -49,13 +48,10 @@ app.get('*', async (req, res) => {
         }
     }
 
-    // if nothing changed !
-    final = final.replace("__DESCRIPTION__","With moviea.tn you can watch free movies online anytime, on any device. It's never been easier to stream movies so get started. | movies | tv shows | movie | free movies | watch free | film complete | film | stream")
-    final = final.replace("__FB_DESCRIPTION__","Watch Movies | Tv-Shows Online Free")
-    final = final.replace('__POSTER__',"https://assets.nflxext.com/ffe/siteui/vlv3/b85863b0-0609-4dba-8fe8-d0370b25b9ee/e7e23acb-d3f4-4c16-87da-22b5e0289506/TN-en-20230731-popsignuptwoweeks-perspective_alpha_website_large.jpg")
-    final = final.replace("__FB_TITLE__","https://assets.nflxext.com/ffe/siteui/vlv3/b85863b0-0609-4dba-8fe8-d0370b25b9ee/e7e23acb-d3f4-4c16-87da-22b5e0289506/TN-en-20230731-popsignuptwoweeks-perspective_alpha_website_large.jpg")
+    
     res.send(final)
 });
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
